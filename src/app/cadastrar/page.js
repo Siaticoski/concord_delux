5b8a9ba659b38939a917dee83414b5cd3e3a374a
errori_aurawell <viniciusavila4080@gmail.com>
2024-12-16 16:07:05 -0400

    a

--- src/app/cadastrar/page.js
+++ src/app/cadastrar/page.js
@@ -1,28 +1,30 @@
 "Use Client"
-import styles from "./page.module.css";
+import styles from "./cadastrar.module.css";
 export default function cadastrar() {
     return (
-        <div id="dFundo">
-            <section>
-                <form action="/submit" method="post">
-                    <label for="nome">Nome:</label>
-                    <input type="text" id="nome" name="nome" required />
+        <section className={styles.main}>
+            <div className={styles.dFundo}>
+                <section className={styles.section}>
+                    <form className={styles.form} action="/submit" method="post">
+                        <label className={styles.label}>Nome:</label>
+                        <input type="text" id="nome" name="nome" required />
 
-                    <label for="senha">Senha:</label>
-                    <input type="senha" id="senha" name="senha" required />
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
+                        <label className={styles.label}>Senha:</label>
+                        <input type="senha" id="senha" name="senha" required />
+                        <label className={styles.label}>Email:</label>
+                        <input type="email" id="email" name="email" required />
 
-                    <label for="celular">Celular:</label>
-                    <input type="celular" id="celular" name="celular" required />
+                        <label className={styles.label} for="celular">Celular:</label>
+                        <input type="celular" id="celular" name="celular" required />
 
-                    <input type="submit" class="botao" value="Entrar" />
-                </form>
+                        <input type="submit" classname={styles.botao} value="Entrar" />
+                    </form>
 
-                <div class="dVoltar">
-                    <p>Você já possui conta? <a href="COLOCAR O LINK DO LOGIN AQUI">Fazer login</a></p>
-                </div>
-            </section>
-        </div>
+                    <div className={styles.dVoltar}>
+                        <p>Você já possui conta? <a href="COLOCAR O LINK DO LOGIN AQUI">Fazer login</a></p>
+                    </div>
+                </section>
+            </div>
+        </section>
     )
 }
