28649e258cc88463a604bc9585a76c85f23aeb61
Siaticoski <caiosiaticoski1@gmail.com>
2025-02-03 16:08:34 -0400

    começo do bloqueio

---
+++ src/app/bloqueio/page.js
@@ -0,0 +1,25 @@
+import styles from "./bloqueio.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
+export default function bloqueio() {
+    return(
+        <selection className={styles.fundo}>
+            <section className={styles.comeco}>
+                <h1>
+                    Bloquear contato e apagar conversa
+                </h1>
+
+                <p>
+
+                </p>
+
+                <div>
+
+                </div>
+
+            </section>
+        </selection>
+        
+    )
+}
