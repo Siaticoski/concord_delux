edaa47a0486edda4236fa09be729cef7b8bc6639
Siaticoski <caiosiaticoski1@gmail.com>
2025-02-04 17:07:40 -0400

    dasdasd

--- src/app/bloqueio/page.js
+++ src/app/bloqueio/page.js
@@ -6,16 +6,26 @@ export default function bloqueio() {
     return(
         <selection className={styles.fundo}>
             <section className={styles.comeco}>
-                <h1>
-                    Bloquear contato e apagar conversa
-                </h1>
 
-                <p>
-
-                </p>
+                <div className={styles.inicio}>
+                    <h1 className={styles.titulo}>
+                        Bloquear contato e apagar conversa
+                    </h1>
+                </div>
+                
 
                 <div>
+                    <p className={styles.texto}>
+                        As últimas mensagems recebidas serão encaminhadas para a equipe Concord. O contato não será notificado.
+                    </p>
+                </div>
 
+                <div className={styles.botoes}>
+
+                    <Link className={styles.lc} href="colocar cancelar"><input type="submit" className={styles.bc} value="Cancelar"/></Link>
+
+                    <Link className={styles.ld} href="colocar denunciar"><input type="submit" className={styles.bd} value="Denunciar"/></Link>
+                    
                 </div>
 
             </section>
